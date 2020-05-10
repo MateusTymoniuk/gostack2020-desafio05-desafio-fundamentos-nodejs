235cfd98f6320e5f694e041c0e96978deecd0c89
Mateus Tymoniuk <tymoniuk.m@gmail.com>
2020-05-10 19:50:46 -0300

    Commit da resolução do desafio

---
+++ README.md
@@ -0,0 +1,35 @@
+<h3 align="center">
+  Desafio 05: Primeiro projeto Node.js
+</h3>
+
+
+<p align="center">
+  <a href="#-sobre">Sobre</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-instalação">Instalação</a>&nbsp;&nbsp;&nbsp;
+</p>
+
+
+## 🚀 **Sobre**
+Esta é uma aplicação feita em NodeJS, que permite criarmos e lsitarmos transações.
+
+Para sua construção utilizamos o conceito de Repositories e Services para retirar do arquivo de routes, a responsabilidade de tratar com as regras de negócio e a comunicação com os dados.
+
+Desafio proposto em: https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-fundamentos-nodejs
+
+## 🚀 **Instalação**
+1 - Clonar o [repositório](https://github.com/MateusTymoniuk/gostack2020-desafio05-desafio-fundamentos-nodejs) em seu computador;
+
+2 - **Instalar as dependências do projeto** digitando no terminal o comando:
+
+    yarn
+
+ou
+
+    npm install
+
+3 - Para **executar a aplicação** utilize:
+
+    yarn dev
+
+ou
+    npm run dev
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,47 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
-class CreateTransactionService {
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+export default class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (this.isTransactionTypeInvalid(type)) {
+      throw Error('Transaction type not allowed.');
+    }
+
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+
+      if (balance.total < value) {
+        throw Error(
+          'Transaction value not allowed: cannot have negative balance',
+        );
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+
+  private isTransactionTypeInvalid(type: string): boolean {
+    if (type === 'income' || type === 'outcome') {
+      return false;
+    }
+    return true;
   }
 }
-
-export default CreateTransactionService;
